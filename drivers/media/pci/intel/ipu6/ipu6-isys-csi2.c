6880c9ff2f37c303bbf84357790e7e4773561b4a
Khai Wen Ng <khai.wen.ng@intel.com>
2025-09-12 04:58:59 +0800

    media: pci: intel: ipu6-isys-csi2: get link frequency from pad
    In case driver does not have v4l2-control that stores link frequency,
    try to retrieve link frequency from pad
    
    Signed-off-by: Khai Wen Ng <khai.wen.ng@intel.com>

--- drivers/media/pci/intel/ipu6/ipu6-isys-csi2.c
+++ drivers/media/pci/intel/ipu6/ipu6-isys-csi2.c
@@ -82,6 +82,7 @@ s64 ipu6_isys_csi2_get_link_freq(struct ipu6_isys_csi2 *csi2)
 	struct media_pad *src_pad;
 	struct v4l2_subdev *ext_sd;
 	struct device *dev;
+	s64 ret;
 
 	if (!csi2)
 		return -EINVAL;
@@ -98,7 +99,16 @@ s64 ipu6_isys_csi2_get_link_freq(struct ipu6_isys_csi2 *csi2)
 	if (WARN(!ext_sd, "Failed to get subdev for %s\n", csi2->asd.sd.name))
 		return -ENODEV;
 
-	return v4l2_get_link_freq(ext_sd->ctrl_handler, 0, 0);
+	ret = v4l2_get_link_freq(ext_sd->ctrl_handler, 0, 0);
+	if (ret) {
+		ret = v4l2_get_link_freq(src_pad, 0, 0);
+		if (ret) {
+			dev_err(dev, "Failed to get link frequency for %s\n",
+				csi2->asd.sd.name);
+			return ret;
+		}
+	}
+	return ret;
 }
 
 static int csi2_subscribe_event(struct v4l2_subdev *sd, struct v4l2_fh *fh,
